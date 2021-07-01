03f7c6ade96fbad652ab76de736377f2c3f4bb29
Rosh-Py <jharoshan.k54@gmail.com>
2021-07-01 12:51:19 +0530

    fixed theme change bug

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import axios from "axios";
 function App() {
   // Fetching all countries on load starts
   const { setAllCountries } = useGlobalContext();
+
+  const savedDarkTheme =
+    localStorage.getItem("darkTheme") === "true" ? true : false;
+  if (savedDarkTheme) {
+    document.documentElement.className = "dark-theme";
+  } else {
+    document.documentElement.className = "light-theme";
+  }
+
   let countries;
   const fetchAllCountries = async (url) => {
     const response = await axios({ url });
